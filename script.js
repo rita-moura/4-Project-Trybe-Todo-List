246525db3a0a718b7d93e182d2b7e7fb35ecd195
Rita Moura <ritamoura520@gmail.com>
2022-12-20 16:07:00 -0300

    Feat: Requisito 7
    Implementado estilo para item marcado como concluído

--- script.js
+++ script.js
@@ -2,15 +2,16 @@ const inputAdd = document.getElementById('texto-tarefa');
 const buttonAdd = document.getElementById('criar-tarefa');
 const olList = document.getElementById('lista-tarefas');
 
-const todoList = [];
-
 buttonAdd.addEventListener('click', () => {
   const liList = document.createElement('li');
   liList.classList = 'item-list';
   liList.innerHTML = inputAdd.value;
   olList.appendChild(liList);
-  todoList.push(liList);
   inputAdd.value = '';
 });
 
-console.log(todoList);
+olList.addEventListener('click', (event) => {
+  if (event.target.tagName === 'LI') {
+    event.target.classList.toggle('done');
+  }
+}, false);
