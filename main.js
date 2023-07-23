59bfc6e2ed772b20dd8e04f7194662d04529563c
Rita Moura <ritamoura520@gmail.com>
2023-07-23 19:32:09 -0300

    Feat: adicona selecionar item da lista um por vez

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const inputAdd = document.getElementById('texto-tarefa');
 const buttonAdd = document.getElementById('criar-tarefa');
 const olList = document.getElementById('lista-tarefas');
 const buttonClear = document.getElementById('apaga-tudo');
+let selectedItem = null;
 
 buttonAdd.addEventListener('click', () => {
   const liList = document.createElement('li');
@@ -12,12 +13,17 @@ buttonAdd.addEventListener('click', () => {
 });
 
 buttonClear.addEventListener('click', () => {
-  olList.innerHTML = '';
+  olList.innerText = '';
 });
+console.log(buttonClear);
 
 olList.addEventListener('click', ({ target }) => {
   if (target.tagName === 'LI') {
-    target.classList.toggle('done');
+    if (selectedItem !== null) {
+      selectedItem.classList.remove('done');
+    }
+    selectedItem = target;
+    selectedItem.classList.toggle('done');
   }
 });
 
